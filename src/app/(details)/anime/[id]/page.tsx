41906272ac9badeae1d518843c3df528265f9055
zafchiel <bialakropla127.9@o2.pl>
2023-07-21 18:58:18 +0200

    desktop details page

--- src/app/(details)/anime/[id]/page.tsx
+++ src/app/(details)/anime/[id]/page.tsx
@@ -1,5 +1,16 @@
 import { redirect } from "next/navigation"
 import type { AnimeInfo } from "@/types"
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Link from "next/link"
 
 export default async function DetailsPage({
   params: { id },
@@ -16,23 +27,56 @@ export default async function DetailsPage({
     redirect("http://localhost:3000")
   }
 
-  const episodeArr = anime.episodes.map(async (an) => {
-    const res = await fetch(
-      `https://api.consumet.org/anime/gogoanime/watch/${an.id}?server=gogocdn`
-    )
-    const urls = await res.json()
-    return urls
-  })
-
   return (
     <main className="w-full">
-      <section className="container flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold uppercase p-4">{anime.title}</h1>
-        <div>
-          {anime.episodes.map((obj) => (
-            <div key={obj.id}>{obj.id}</div>
-          ))}
+      <section className="container flex flex-col items-center">
+        <div className="flex h-full">
+          <Image
+            src={anime.image}
+            width={400}
+            height={500}
+            alt={anime.title}
+            className="aspect-[4/5]"
+          />
+          <div className="flex flex-col justify-start p-4">
+            <div className="flex">
+              <h1 className="text-4xl font-bold uppercase">{anime.title}</h1>
+              <p className="ml-1">{anime.releaseDate}</p>
+            </div>
+            <p className="opacity-70">{anime.otherName}</p>
+
+            <p className="mt-2">{anime.description}</p>
+            <div className="flex opacity-70 gap-3">
+              {anime.genres.map((obj) => (
+                <p key={obj}>{obj}</p>
+              ))}
+            </div>
+          </div>
         </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="md:w-3/4 m-4 w-full">Watch Now</Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Pick your poison</SheetTitle>
+              <SheetDescription>
+                choosing will redirect you to episode page
+              </SheetDescription>
+            </SheetHeader>
+            <div className="grid grid-cols-3 sm:grid-cols-5 gap-2 mt-2">
+              {anime.episodes.map((obj) => (
+                <Link
+                  href={`anime/watch/${obj.id}`}
+                  key={obj.id}
+                  className="p-4 border flex justify-center items-center rounded-lg hover:bg-secondary"
+                >
+                  {obj.number}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
       </section>
     </main>
   )
---
+++ src/app/(details)/anime/watch/[episode-id]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function EpisodePage() {
+  return <div>EpisodePage</div>
+}
