d2964c9172d6040da15b8abd2e5f85b897ad1857
zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-07-26 06:52:03 +0000

    sync

--- src/app/(details)/[id]/watch/[episode-id]/page.tsx
+++ src/app/(details)/[id]/watch/[episode-id]/page.tsx
@@ -1,9 +1,24 @@
-import Player from "@/components/episodePage/player";
+import Player from "@/components/episodePage/player"
+import EpisodeList from "@/components/detailsPage/EpisodeLIst"
 
-export default function EpisodePage() {
+export default function EpisodePage({params: {id, episode-id}}: {params: {id: string, episode-id: string}}) {
+  // Search anime by slug
+  const search = await fetch(`https://api.consumet.org/anime/gogoanime/${id}`)
+  const searchResults = await search.json()
+
+  // Fetch detailed info of first record found
+  const res = await fetch(
+    `https://api.consumet.org/anime/gogoanime/info/${searchResults.results[0].id}`
+  )
+  const anime: AnimeInfo = await res.json()
+
+  if (Object.keys(anime).length === 1) {
+    redirect("http://localhost:3000")
+  }
   return (
     <div className="flex items-center justify-center h-screen w-full">
       <Player />
+      <EpisodeList />
     </div>
   )
 }
