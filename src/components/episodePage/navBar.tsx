1b67818e47af98ba2ca5a2a2372984169f678aa6
zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-07-27 11:44:36 +0000

    getting next and pev ep

--- src/components/episodePage/navBar.tsx
+++ src/components/episodePage/navBar.tsx
@@ -4,21 +4,30 @@ import { extractEpisodeNumber } from "@/lib/utils"
 import { episodeListAtom } from "../detailsPage/EpisodeLIst"
 import { useAtomValue } from "jotai"
 import { StepBack, StepForward } from "lucide-react"
-import { useParams } from "next/navigation"
+import { useParams, usePathname, useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function NavBar() {
-  const epArr = useAtomValue(episodeListAtom)
+  const { id, episode_id } = useParams()
+  const pathname = usePathname()
+  const router = useRouter()
 
-  const { episode_id } = useParams()
+  const epArr = useAtomValue(episodeListAtom)
   const epNumber = extractEpisodeNumber(episode_id)
 
-  if (typeof epNumber === "number") {
-    console.log(epNumber)
-  }
+  useEffect(() => {
+    if (typeof epNumber === "number") {
+      const nextEp = episode_id.slice(0, -epNumber.toString().length) + (epNumber + 1)
+      const prevEp = episode_id.slice(0, -epNumber.toString().length) + (epNumber - 1)
+
+      console.log(nextEp)
+      console.log(prevEp)
+    }
+  }, [])
 
   return (
     <div className="fixed bottom-0 inset-x-0 h-40 bg-primary-foreground">
-      <StepBack />
+      <StepBack onClick={() => router.push(`/${id}/`)} />
     </div>
   )
 }
