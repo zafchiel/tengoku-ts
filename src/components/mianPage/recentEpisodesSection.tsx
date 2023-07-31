65a8a676ec7c7f8448418d55c17879c8db25f4b1
zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-07-31 05:51:34 +0000

    fetch new episodes on scroll

--- src/components/mianPage/recentEpisodesSection.tsx
+++ src/components/mianPage/recentEpisodesSection.tsx
@@ -2,7 +2,7 @@
 
 import { RecentEpisodesResponseSchema } from "@/types"
 import RecentEpisodeCard from "./recentEpisodeCard"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef, useCallback } from "react"
 import { Button } from "../ui/button"
 import { Loader2 } from "lucide-react"
 import axios from "axios"
@@ -15,33 +15,63 @@ export default function RecentEpisodesSection({ episodes }: Props) {
   const [recentEpisodes, setRecentEpisodes] = useState(episodes.results)
   const [currentPage, setCurrentPage] = useState(2)
   const [loading, setLoading] = useState(false)
+  const [hasNextPage, setHasNextPage] = useState(true)
+
+  const [inView, setInView] = useState(false)
+  const ref = useRef<HTMLButtonElement | null>(null)
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.7,
+  }
+
+  
+  const fetchMoreEpisodes = async () => {
+    setLoading(true)
+    if(!hasNextPage) {
+      console.log('No More :3')
+      return
+    }
+    try {
+      const {data} = await axios.get('https://api.consumet.org/anime/gogoanime/recent-episodes', {params: {page: currentPage}})
+      setRecentEpisodes((prev) => [...prev, ...data.results])
+      setCurrentPage((prev) => prev + 1)
+      if(!data.hasNextPage) setHasNextPage(false)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
-    const fetchMoreEpisodes = async () => {
-      setLoading(true)
-      try {
-        const {data} = await axios.get('https://api.consumet.org/anime/gogoanime/recent-episodes', {params: {page: currentPage}})
-        setRecentEpisodes((prev) => [...prev, ...data.results])
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setLoading(false)
-      }
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries
+      setInView(entry.isIntersecting)
+      if(entry.isIntersecting === true && loading === false) fetchMoreEpisodes()
+    }, observerOptions)
+
+    if(ref.current) observer.observe(ref.current)
+
+    return () => {
+      if(ref.current) observer.unobserve(ref.current)
     }
-    fetchMoreEpisodes()
-  }, [currentPage])
+  }, [ref, observerOptions])
+
 
   return (
     <>
       <h1 className="text-3xl p-5 font-bold">Recently added episodes</h1>
 
-      <section className="w-full p-5 flex flex-wrap">
-        {recentEpisodes.map((obj) => (
+      <section  className="w-full p-5 flex flex-wrap">
+        {recentEpisodes.map((obj, index) => (
           <RecentEpisodeCard key={obj.episodeId} ep={obj} />
         ))}
       </section>
       <div className="p-3">
         <Button
+        ref={ref}
           disabled={loading}
           onClick={() => {
             setCurrentPage((prev) => prev + 1)
