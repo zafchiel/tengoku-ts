97c00e47466f8f486cf7971cb357880b710e86c8
zafchiel <bialakropla127.9@o2.pl>
2023-07-31 19:39:39 +0200

    pwa commented out, not working in development

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-const withPWA = require("next-pwa")({
+const withPWAInit = require("next-pwa")
+
+/** @type {import('next-pwa').PWAConfig} */
+const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
+  // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
+  buildExcludes: ["app-build-manifest.json"],
 })
 
 /** @type {import('next').NextConfig} */
