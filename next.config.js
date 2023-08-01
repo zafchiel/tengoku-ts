b8f40ea723b159dcbeb204d9ff06d9d86c518f77
zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-08-01 08:17:43 +0000

    pwa production test

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-// const withPWAInit = require("next-pwa")
+// PWA SUPPORT
 
-// /** @type {import('next-pwa').PWAConfig} */
-// const withPWA = withPWAInit({
-//   dest: "public",
-//   disable: process.env.NODE_ENV === "development",
-//   register: true,
-//   // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
-//   buildExcludes: ["app-build-manifest.json"],
-// })
+const withPWAInit = require("next-pwa")
+
+/** @type {import('next-pwa').PWAConfig} */
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
+  buildExcludes: ["app-build-manifest.json"],
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -16,5 +18,17 @@ const nextConfig = {
   },
 }
 
-// module.exports = withPWA(nextConfig)
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
+
+
+
+// WITHOUT PWA
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["img.youtube.com", "cdn.myanimelist.net", "gogocdn.net"],
+//   },
+// }
+
+// module.exports = nextConfig
