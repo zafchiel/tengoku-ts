c91e1b611a4a2d65ba2a848fb086803584c20388
zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-08-01 04:57:04 +0000

    pwa commented out again

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const withPWAInit = require("next-pwa")
+// const withPWAInit = require("next-pwa")
 
-/** @type {import('next-pwa').PWAConfig} */
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
-  buildExcludes: ["app-build-manifest.json"],
-})
+// /** @type {import('next-pwa').PWAConfig} */
+// const withPWA = withPWAInit({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === "development",
+//   register: true,
+//   // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
+//   buildExcludes: ["app-build-manifest.json"],
+// })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -16,4 +16,5 @@ const nextConfig = {
   },
 }
 
-module.exports = withPWA(nextConfig)
+// module.exports = withPWA(nextConfig)
+module.exports = nextConfig
