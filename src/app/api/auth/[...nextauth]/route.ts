17f8c1b7aea15dc4a7c8bf170474e097b6ac017b
zafchiel <bialakropla127.9@o2.pl>
2023-08-02 16:23:03 +0200

    google OAuth provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,17 @@
 import { AuthOptions } from "next-auth"
 import NextAuth from "next-auth/next"
 import RedditProvider from "next-auth/providers/reddit"
+import GoogleProvider from "next-auth/providers/google"
 
 const authConfig: AuthOptions = {
   providers: [
     RedditProvider({
       clientId: process.env.REDDIT_CLIENT_ID as string,
       clientSecret: process.env.REDDIT_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 }
