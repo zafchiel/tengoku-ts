c3fbf0102ecf95e8e48530e10ac96c097898ba1c
zafchiel <bialakropla127.9@o2.pl>
2023-08-08 10:24:17 +0200

    form

--- src/components/profilePage/form.tsx
+++ src/components/profilePage/form.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,57 +13,106 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { Switch } from "../ui/switch"
+} from "@/components/ui/select";
+import { Input } from "../ui/input";
 
 const formSchema = z.object({
-  is_completed: z.boolean(),
+  status: z.string(),
   score: z.number().int().min(1).max(2),
   progress: z.number().int().min(1),
-})
+});
 
 export default function FormComponment() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      is_completed: false,
+      status: "Watching",
       score: 0,
       progress: 0,
     },
-  })
+  });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <FormField
           control={form.control}
-          name="is_completed"
+          name="status"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Status</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue="Watching">
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="State of progress" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="Completed">Completed</SelectItem>
+                  <SelectItem value="Watching">Watching</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>Mamale</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="score"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Score</FormLabel>
               <FormControl>
-                <Switch
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
+                <Input
+                  placeholder="Score"
+                  type="number"
+                  min={1}
+                  max={10}
+                  {...field}
                 />
               </FormControl>
+              <FormDescription>Yout opinionated score</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="progress"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Progress</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Progress"
+                  type="number"
+                  min={0}
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                How many episodes have you watched?
+              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
       </form>
     </Form>
-  )
+  );
 }
