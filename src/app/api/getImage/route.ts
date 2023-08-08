02a7a08a4d7e121815355fd23ccb2bc059e8ab29
zafchiel <bialakropla127.9@o2.pl>
2023-08-08 11:39:24 +0200

    validate request

--- src/app/api/getImage/route.ts
+++ src/app/api/getImage/route.ts
@@ -1,8 +1,20 @@
-import getBase64 from "@/lib/getBase64Image"
-import { NextResponse } from "next/server"
+import getBase64 from "@/lib/getBase64Image";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+const requestSchema = z.object({
+  img: z.string().url(),
+});
 
 export async function POST(request: Request) {
-  const body = await request.json()
-  const img = await getBase64(body.img)
-  return NextResponse.json(img)
+  const body = await request.json();
+  try {
+    requestSchema.parse(body);
+  } catch (error) {
+    console.log(error);
+  }
+
+  const img = await getBase64(body.img);
+
+  return NextResponse.json(img);
 }
