70ce1badbe7a979320ec8d17733e012e61d29103
zafchiel <bialakropla127.9@o2.pl>
2023-08-09 10:56:29 +0200

    params type error fix

--- src/app/(details)/[id]/watch/[episode_id]/page.tsx
+++ src/app/(details)/[id]/watch/[episode_id]/page.tsx
@@ -1,66 +1,66 @@
-import Link from "next/link"
-import Player from "@/components/episodePage/player"
-import NavBar from "@/components/episodePage/navBar"
-import { extractNameAndEpisode } from "@/lib/utils"
-import { EpisodesRecord, SourcesRecord, getXataClient } from "@/xata/xata"
-import { redirect } from "next/navigation"
-import { insertNewAnime, updateEpisodesInDb } from "@/xata/anime"
-import { fetchAnimeInfo } from "@/lib/utils"
+import Link from "next/link";
+import Player from "@/components/episodePage/player";
+import NavBar from "@/components/episodePage/navBar";
+import { extractNameAndEpisode } from "@/lib/utils";
+import { EpisodesRecord, SourcesRecord, getXataClient } from "@/xata/xata";
+import { redirect } from "next/navigation";
+import { insertNewAnime, updateEpisodesInDb } from "@/xata/anime";
+import { fetchAnimeInfo } from "@/lib/utils";
 
 type Props = {
   params: {
-    id: string
-    episode_id: string
-  }
-}
+    id: string;
+    episode_id: string;
+  };
+};
 
 export default async function EpisodePage({ params }: Props) {
   // Fetch anime info for episode list
-  const anime = await fetchAnimeInfo(params.id)
+  const anime = await fetchAnimeInfo(params.id);
 
-  const xata = getXataClient()
-  const animeDB = await xata.db.animes.read(anime.id)
+  const xata = getXataClient();
+  const animeDB = await xata.db.animes.read(anime.id);
 
   if (animeDB) {
     const episodesInDB = await xata.db.episodes
       .filter({ anime_id: anime.id })
-      .getMany()
+      .getMany();
     if (episodesInDB.length !== anime.totalEpisodes) {
-      await updateEpisodesInDb(anime, animeDB.totalEpisodes!)
+      await updateEpisodesInDb(anime, animeDB.totalEpisodes!);
     }
   }
 
   if (!animeDB) {
-    await insertNewAnime(anime)
+    await insertNewAnime(anime);
   }
 
   const srcs = await xata.db.sources
     .filter({ episode_id: params.episode_id })
-    .getMany()
+    .getMany();
 
-  const { name, episode } = extractNameAndEpisode(params.episode_id)
+  const { name, episode } = extractNameAndEpisode(params.episode_id);
 
   const sources = srcs.map((obj) => ({
     isM3U8: obj.isM3U8,
     url: obj.url,
     quality: obj.quality,
-  }))
+  }));
 
   const epsArr = await xata.db.episodes
     .filter({ anime_id: params.id })
-    .getMany()
+    .getMany();
 
   const eps = epsArr.map((obj) => ({
     id: obj.id,
     number: obj.number,
-  }))
+  }));
 
-  if (srcs === null) redirect(`/${params.id}`)
+  if (srcs === null) redirect(`/${params.id}`);
 
   return (
     <div className="flex items-center flex-col justify-center w-full py-14 overflow-x-hidden">
       <Player urls={sources as SourcesRecord[]} />
-      <NavBar episodeList={eps as EpisodesRecord[]} />
+      <NavBar episodeList={eps as EpisodesRecord[]} params={params} />
       <div className="mt-3 p-2 w-full">
         <h3 className="text-2xl uppercase font-semibold">
           <Link href={`/${name}`}>{name}</Link>
@@ -68,5 +68,5 @@ export default async function EpisodePage({ params }: Props) {
         <p>EP:&nbsp;{episode}</p>
       </div>
     </div>
-  )
+  );
 }
--- src/components/episodePage/navBar.tsx
+++ src/components/episodePage/navBar.tsx
@@ -1,33 +1,36 @@
-"use client"
+"use client";
 
-import { cn, extractEpisodeNumber } from "@/lib/utils"
-import Link from "next/link"
-import EpisodeList from "../detailsPage/EpisodeLIst"
-import { StepBack, StepForward } from "lucide-react"
-import { useParams, useRouter } from "next/navigation"
-import { Button } from "../ui/button"
-import { EpisodeListType } from "@/types"
-import { EpisodesRecord } from "@/xata/xata"
+import { cn, extractEpisodeNumber } from "@/lib/utils";
+import Link from "next/link";
+import EpisodeList from "../detailsPage/EpisodeLIst";
+import { StepBack, StepForward } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import { EpisodeListType } from "@/types";
+import { EpisodesRecord } from "@/xata/xata";
 
 type Props = {
-  episodeList: EpisodeListType | EpisodesRecord[]
-}
+  episodeList: EpisodeListType | EpisodesRecord[];
+  params: {
+    id: string;
+    episode_id: string;
+  };
+};
 
-export default function NavBar({ episodeList }: Props) {
-  const { id, episode_id } = useParams()
-  const router = useRouter()
+export default function NavBar({ episodeList, params }: Props) {
+  const router = useRouter();
 
-  const epNumber = extractEpisodeNumber(episode_id)
+  const epNumber = extractEpisodeNumber(params.episode_id);
 
   const nextEp =
-    episode_id.slice(0, -epNumber.toString().length) + (epNumber + 1)
+    params.episode_id.slice(0, -epNumber.toString().length) + (epNumber + 1);
   const prevEp =
-    episode_id.slice(0, -epNumber.toString().length) + (epNumber - 1)
+    params.episode_id.slice(0, -epNumber.toString().length) + (epNumber - 1);
 
   return (
     <div className="flex fixed bottom-0 inset-x-0 md:static w-full h-20 justify-around items-center">
       <Link
-        href={`/${id}/watch/${prevEp}`}
+        href={`/${params.id}/watch/${prevEp}`}
         className={cn(
           "cursor-pointer py-1 px-5 hover:bg-accent hover:text-accent-foreground rounded-md",
           {
@@ -44,7 +47,7 @@ export default function NavBar({ episodeList }: Props) {
         </Button>
       </EpisodeList>
       <Link
-        href={`/${id}/watch/${nextEp}`}
+        href={`/${params.id}/watch/${nextEp}`}
         className={cn(
           "cursor-pointer py-1 px-5 hover:bg-accent hover:text-accent-foreground rounded-md",
           {
@@ -56,5 +59,5 @@ export default function NavBar({ episodeList }: Props) {
         <StepForward size={34} />
       </Link>
     </div>
-  )
+  );
 }
