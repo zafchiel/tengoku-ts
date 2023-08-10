8900b59e9b96fc9d5a3ab19595bc19cca8b2b1f4
zafchiel <bialakropla127.9@o2.pl>
2023-08-10 13:17:15 +0200

    code cleanup

--- src/app/(user)/user/[user_id]/page.tsx
+++ src/app/(user)/user/[user_id]/page.tsx
@@ -6,16 +6,14 @@ import ProgressSection from "@/components/profilePage/progressSection"
 export default async function ProfilePage() {
   const session = await getServerSession(authConfig)
 
-  if (!session || !session.user) redirect(`/api/auth/signin`)
+  if (!session?.user) redirect(`/api/auth/signin`)
 
   return (
     <main className="p-3 flex flex-col">
-      <div className="w-full pt-14 text-right">
-        <section>
-          <h1 className="text-6xl font-bold">{session.user?.name}</h1>
-          <p className="text-muted-foreground">{session.user?.email}</p>
-        </section>
-      </div>
+      <section className="pt-14 w-full text-right">
+        <h1 className="text-6xl font-bold">{session.user?.name}</h1>
+        <p className="text-muted-foreground">{session.user?.email}</p>
+      </section>
       <div>
         <section className="mt-5">
           <h3 className="text-3xl font-bold">Currenlty watching</h3>
