5729f6eeb8e78bf136a1b95b0eadc1359ba9462c
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-08-13 18:02:19 +0200

    fix: corrertly fetch new episode

--- src/app/(details)/[id]/watch/[episode_id]/page.tsx
+++ src/app/(details)/[id]/watch/[episode_id]/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import Player from "@/components/episodePage/player"
 import NavBar from "@/components/episodePage/navBar"
-import { extractNameAndEpisode } from "@/lib/utils"
+import { extractNameAndEpisode, fetchSource } from "@/lib/utils"
 import { EpisodesRecord, SourcesRecord, getXataClient } from "@/xata/xata"
 import { redirect } from "next/navigation"
 import { insertNewAnime, updateEpisodesInDb } from "@/xata/anime"
@@ -34,33 +34,15 @@ export default async function EpisodePage({ params }: Props) {
     await insertNewAnime(anime)
   }
 
-  const srcs = await xata.db.sources
-    .filter({ episode_id: params.episode_id })
-    .getMany()
+  const sourcesArray = await fetchSource(params.episode_id)
+  if (sourcesArray === null) redirect(`/${params.id}`)
 
   const { name, episode } = extractNameAndEpisode(params.episode_id)
 
-  const sources = srcs.map((obj) => ({
-    isM3U8: obj.isM3U8,
-    url: obj.url,
-    quality: obj.quality,
-  }))
-
-  const epsArr = await xata.db.episodes
-    .filter({ anime_id: params.id })
-    .getMany()
-
-  const eps = epsArr.map((obj) => ({
-    id: obj.id,
-    number: obj.number,
-  }))
-
-  if (srcs === null) redirect(`/${params.id}`)
-
   return (
     <div className="flex items-center flex-col justify-center w-full py-14 overflow-x-hidden">
-      <Player urls={sources as SourcesRecord[]} />
-      <NavBar episodeList={eps as EpisodesRecord[]} params={params} />
+      <Player urls={sourcesArray as SourcesRecord[]} />
+      <NavBar episodeList={anime.episodes} params={params} />
       <div className="mt-3 p-2 w-full">
         <h3 className="text-2xl uppercase font-semibold">
           <Link href={`/${name}`}>{name}</Link>
