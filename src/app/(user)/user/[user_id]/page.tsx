204cca0c44ad9f25618c41e397944dd8f7dd4cb1
zafchiel <bialakropla127.9@o2.pl>
2023-08-14 14:50:21 +0200

    layout

--- src/app/(user)/user/[user_id]/page.tsx
+++ src/app/(user)/user/[user_id]/page.tsx
@@ -1,16 +1,16 @@
-import { redirect } from "next/navigation"
-import { authConfig } from "@/pages/api/auth/[...nextauth]"
-import { getServerSession } from "next-auth"
-import ProgressSection from "@/components/profilePage/progressSection"
+import { redirect } from "next/navigation";
+import { authConfig } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import ProgressSection from "@/components/profilePage/progressSection";
 
 export default async function ProfilePage() {
-  const session = await getServerSession(authConfig)
+  const session = await getServerSession(authConfig);
 
-  if (!session?.user) redirect(`/api/auth/signin`)
+  if (!session?.user) redirect(`/api/auth/signin`);
 
   return (
     <main className="p-3 flex flex-col">
-      <section className="pt-14 w-full text-right">
+      <section className="w-full text-right">
         <h1 className="text-6xl font-bold">{session.user?.name}</h1>
         <p className="text-muted-foreground">{session.user?.email}</p>
       </section>
@@ -21,5 +21,5 @@ export default async function ProfilePage() {
         </section>
       </div>
     </main>
-  )
+  );
 }
