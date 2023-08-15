049ddbcf1d9384cde7a48254a76cc76a46768b6d
zafchiel <bialakropla127.9@o2.pl>
2023-08-15 11:24:13 +0200

    episode page follow button

--- src/components/episodePage/markAsWatchedButton.tsx
+++ src/components/episodePage/markAsWatchedButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import { useToast } from "../ui/use-toast";
@@ -18,6 +18,9 @@ export default function MarkAsWatchedButton({
   animeLength,
 }: Props) {
   const [markedAsWatched, setMarkedAsWatched] = useState(false);
+  const [isFollowed, setIsFollowed] = useState(false);
+  const [userProgress, setUserProgess] = useState(null);
+
   const { toast } = useToast();
   const { data: session } = useSession();
 
@@ -72,16 +75,73 @@ export default function MarkAsWatchedButton({
       });
     }
   };
+
+  useEffect(() => {
+    const checkIfAnimeIsFollowed = async () => {
+      if (!session?.user?.id) {
+        return;
+      }
+      try {
+        const { data } = await axios.get("/api/user/getProgress", {
+          params: {
+            anime_id,
+            user_id: session.user.id,
+          },
+        });
+        if (data) {
+          setUserProgess(data);
+          setIsFollowed(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    checkIfAnimeIsFollowed();
+  }, [anime_id, session?.user, markedAsWatched]);
+
+  const handleFollow = async () => {
+    if (isFollowed) return;
+    if (!session?.user?.id) {
+      toast({
+        description: "You need to be logged in to mark episodes as watched",
+      });
+      return;
+    }
+    if (userProgress) return;
+
+    try {
+      const { data } = await axios.post("/api/anime/follow", {
+        anime: anime_id,
+        user: session.user.id,
+      });
+      setIsFollowed(true);
+      setUserProgess(data);
+    } catch (error) {
+      console.log(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong",
+      });
+    }
+  };
+
   return (
-    <Button
-      variant="outline"
-      disabled={markedAsWatched}
-      onClick={handleButtonClick}
-      className={cn("w-full p-4 uppercase font-semibold", {
-        "bg-muted-foreground": markedAsWatched,
-      })}
-    >
-      Mark as watched to this point
-    </Button>
+    <div className="flex">
+      <Button
+        variant="secondary"
+        disabled={markedAsWatched}
+        onClick={handleButtonClick}
+        className={cn("w-full p-4 uppercase font-semibold rounded-r-none")}
+      >
+        Mark as watched to this point
+      </Button>
+      <Button
+        disabled={isFollowed}
+        onClick={handleFollow}
+        className="rounded-l-none"
+      >
+        {isFollowed ? "Followed" : "Follow"}
+      </Button>
+    </div>
   );
 }
