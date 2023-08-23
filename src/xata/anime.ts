62340f6d7d29997800188bc1efa1339d9248746d
zafchiel <bialakropla127.9@o2.pl>
2023-08-23 18:21:16 +0200

    typos

--- src/xata/anime.ts
+++ src/xata/anime.ts
@@ -1,60 +1,60 @@
-import { getXataClient } from "./xata"
-import { AnimeInfo } from "@/types"
-import { fetchSource } from "@/lib/utils"
+import { getXataClient } from "./xata";
+import { AnimeInfo } from "@/types";
+import { fetchSource } from "@/lib/utils";
 
 export async function insertNewAnime(anime: AnimeInfo) {
-  const xata = getXataClient()
+  const xata = getXataClient();
 
-  const { episodes, ...rest } = anime
+  const { episodes, ...rest } = anime;
   // Create anime record
-  await xata.db.animes.create({ ...rest })
+  await xata.db.animes.create({ ...rest });
 
   // Create episodes records
-  const eps = episodes.map((ep) => ({ anime_id: anime.id, ...ep }))
-  const epsArr = await xata.db.episodes.create([...eps])
+  const eps = episodes.map((ep) => ({ anime_id: anime.id, ...ep }));
+  const epsArr = await xata.db.episodes.create([...eps]);
 
-  // Create souces records
+  // Create sources records
   epsArr.map((ep) => {
     fetchSource(ep.id).then((res) => {
       res.map(async (srcObj) => {
         try {
-          await xata.db.sources.create({ episode_id: ep.id, ...srcObj })
+          await xata.db.sources.create({ episode_id: ep.id, ...srcObj });
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
-      })
-    })
-  })
+      });
+    });
+  });
 }
 
 export async function updateEpisodesInDb(
   anime: AnimeInfo,
-  currentEpisodesNumber: number = 0
+  currentEpisodesNumber: number = 0,
 ) {
-  const xata = getXataClient()
+  const xata = getXataClient();
 
   // Update anime record
   await xata.db.animes.update(anime.id, {
     totalEpisodes: anime.totalEpisodes,
     status: anime.status,
-  })
+  });
 
   // Update episodes records
   const eps = anime.episodes
     .slice(currentEpisodesNumber - 1)
-    .map((ep) => ({ anime_id: anime.id, ...ep }))
-  const epsArr = await xata.db.episodes.create([...eps])
+    .map((ep) => ({ anime_id: anime.id, ...ep }));
+  const epsArr = await xata.db.episodes.create([...eps]);
 
-  // Create souces records
+  // Create sources records
   epsArr.map((ep) => {
     fetchSource(ep.id).then((res) => {
       res.map(async (srcObj) => {
         try {
-          await xata.db.sources.create({ episode_id: ep.id, ...srcObj })
+          await xata.db.sources.create({ episode_id: ep.id, ...srcObj });
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
-      })
-    })
-  })
+      });
+    });
+  });
 }
