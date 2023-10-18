4f5eb67828394de29edd169d5ee88687ab745b7f
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-10-18 16:16:45 +0200

    infinite scroll search results

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,37 +1,107 @@
 "use client";
 
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { SearchResult } from "@/types";
+import { Button } from "@/components/ui/button";
 import SearchResultCard from "@/components/mianPage/searchResultCard";
+import { Loader2 } from "lucide-react";
 
 export default function SearchResultsPage() {
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  const ref = useRef<HTMLButtonElement | null>(null);
+
+  const router = useRouter();
+
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7,
+  };
 
   const searchParams = useSearchParams()!;
   const query = searchParams.get("query");
+  const currentPage = searchParams.get("page");
+  const [queryController, setQueryController] = useState(query);
 
   useEffect(() => {
     const fetchSearchResults = async () => {
+      setIsLoading(true);
+      if (!hasNextPage) {
+        console.log("No More :3");
+        return;
+      }
+      if (queryController !== query) setSearchResults([]);
       try {
         const { data } = await axios.get(
-          `https://api.consumet.org/anime/gogoanime/${query}`
+          `https://api.consumet.org/anime/gogoanime/${query}?page=${currentPage}`
         );
-        setSearchResults(data.results);
+        setSearchResults((prev) => [...prev, ...data.results]);
+        if (!data.hasNextPage) setHasNextPage(false);
+        setQueryController(query!);
       } catch (error) {
         console.log(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchSearchResults();
-  }, [query]);
+  }, [query, currentPage]);
+
+  const navigateToNextPage = () => {
+    router.replace(`/search?query=${query}&page=${Number(currentPage)! + 1}`, {
+      scroll: false,
+    });
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      if (entry.isIntersecting === true && isLoading === false)
+        navigateToNextPage();
+    }, observerOptions);
+
+    const refElement = ref.current;
+
+    if (refElement) observer.observe(refElement);
+
+    return () => {
+      if (refElement) observer.unobserve(refElement);
+    };
+  }, [ref, observerOptions, isLoading]);
 
   return (
-    <main className="w-full pb-14 md:pb-5 md:pt-14 p-5 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-3">
-      {searchResults.map((e) => (
-        <SearchResultCard key={e.id} ep={e} />
-      ))}
+    <main>
+      <section className="w-full pb-14 md:pb-5 md:pt-14 p-5 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-3">
+        {searchResults.map((e) => (
+          <SearchResultCard key={e.id} ep={e} />
+        ))}
+      </section>
+      <div className="p-3">
+        <Button
+          ref={ref}
+          disabled={isLoading || !hasNextPage}
+          onClick={() => navigateToNextPage()}
+          className="w-full"
+        >
+          {!hasNextPage ? (
+            <p>No More</p>
+          ) : isLoading ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Loading
+            </>
+          ) : (
+            <p>Load More</p>
+          )}
+        </Button>
+      </div>
     </main>
   );
 }
--- src/components/mianPage/recentEpisodesSection.tsx
+++ src/components/mianPage/recentEpisodesSection.tsx
@@ -1,62 +1,62 @@
-"use client"
+"use client";
 
-import { RecentEpisode } from "@/types"
-import RecentEpisodeCard from "./recentEpisodeCard"
-import { useState, useEffect, useRef } from "react"
-import { Button } from "../ui/button"
-import { Loader2 } from "lucide-react"
-import axios from "axios"
+import { RecentEpisode } from "@/types";
+import RecentEpisodeCard from "./recentEpisodeCard";
+import { useState, useEffect, useRef } from "react";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+import axios from "axios";
 
 export default function RecentEpisodesSection() {
-  const [recentEpisodes, setRecentEpisodes] = useState<RecentEpisode[]>([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [loading, setLoading] = useState(false)
-  const [hasNextPage, setHasNextPage] = useState(true)
+  const [recentEpisodes, setRecentEpisodes] = useState<RecentEpisode[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasNextPage, setHasNextPage] = useState(true);
 
-  const ref = useRef<HTMLButtonElement | null>(null)
+  const ref = useRef<HTMLButtonElement | null>(null);
 
   const observerOptions = {
     root: null,
     rootMargin: "0px",
     threshold: 0.7,
-  }
+  };
 
   const fetchMoreEpisodes = async () => {
-    setLoading(true)
+    setLoading(true);
     if (!hasNextPage) {
-      console.log("No More :3")
-      return
+      console.log("No More :3");
+      return;
     }
     try {
       const { data } = await axios.get(
         "https://api.consumet.org/anime/gogoanime/recent-episodes",
         { params: { page: currentPage } }
-      )
-      setRecentEpisodes((prev) => [...prev, ...data.results])
-      setCurrentPage((prev) => prev + 1)
-      if (!data.hasNextPage) setHasNextPage(false)
+      );
+      setRecentEpisodes((prev) => [...prev, ...data.results]);
+      setCurrentPage((prev) => prev + 1);
+      if (!data.hasNextPage) setHasNextPage(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
-      const [entry] = entries
+      const [entry] = entries;
       if (entry.isIntersecting === true && loading === false)
-        fetchMoreEpisodes()
-    }, observerOptions)
+        fetchMoreEpisodes();
+    }, observerOptions);
 
-    const refElement = ref.current
+    const refElement = ref.current;
 
-    if (refElement) observer.observe(refElement)
+    if (refElement) observer.observe(refElement);
 
     return () => {
-      if (refElement) observer.unobserve(refElement)
-    }
-  }, [ref, observerOptions, loading])
+      if (refElement) observer.unobserve(refElement);
+    };
+  }, [ref, observerOptions, loading]);
 
   return (
     <>
@@ -85,5 +85,5 @@ export default function RecentEpisodesSection() {
         </Button>
       </div>
     </>
-  )
+  );
 }
