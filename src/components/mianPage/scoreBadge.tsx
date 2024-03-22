cc26b21e18bafddda88586694199f85526769525
zafchiel <bialakropla@proton.me>
2024-03-22 17:36:44 +0100

    fix build errors

--- src/components/mianPage/scoreBadge.tsx
+++ src/components/mianPage/scoreBadge.tsx
@@ -9,10 +9,12 @@ export default function ScoreBadge({ score }: Props) {
   return (
     <Badge
       variant="secondary"
-      className="!border-none flex justify-center items-center gap-2 text-md"
+      className="!border-none flex justify-center items-center gap-2 text-md px-2 py-1 !rounded-full"
     >
-      <Star size={12} />
-      {score}
+      <Star size={18} />
+      <span>
+        {score}
+      </span>
     </Badge>
   )
 }
