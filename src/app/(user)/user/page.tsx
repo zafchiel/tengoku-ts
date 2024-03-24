b8ed91ca4c54a3a175dd971fd61a96561d45c4de
zafchiel <bialakropla@proton.me>
2024-03-24 20:53:14 +0100

    Refactor user profile page layout

--- src/app/(user)/user/page.tsx
+++ src/app/(user)/user/page.tsx
@@ -12,15 +12,10 @@ export default async function ProfilePage() {
 
   return (
     <main className={cn("p-3 flex flex-col md:pt-14 container")}>
-      <section className="flex items-center justify-between">
+      <section className="flex flex-wrap items-center justify-between">
         <h1 className="text-6xl font-bold">{user.username}</h1>
         <LogoutForm />
       </section>
-      <div>
-        <section className="mt-5">
-          {/* <ProgressSection user={session.user} /> */}
-        </section>
-      </div>
     </main>
   );
 }
