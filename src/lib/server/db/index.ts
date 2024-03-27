3d877aae35219bf5d9dc6a50792e6532bfe48ecb
zafchiel <bialakropla127.9@o2.pl>
2024-03-27 19:21:30 +0100

    update example env

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -7,13 +7,13 @@ const dev = process.env.NODE_ENV === 'development';
 const TURSO_DB_URL = process.env.TURSO_DB_URL;
 const TURSO_DB_SECRET_TOKEN = process.env.TURSO_DB_SECRET_TOKEN;
 
-if(!TURSO_DB_URL) {
+if(!TURSO_DB_URL && !dev) {
     throw new Error('TURSO_DB_URL is not defined');
 }
 
-if(!TURSO_DB_SECRET_TOKEN) {
+if(!TURSO_DB_SECRET_TOKEN && !dev) {
     throw new Error('TURSO_DB_SECRET_TOKEN is not defined');
 }
 
-const sqlite = createClient({url: dev ? "file:sqlite.db" : TURSO_DB_URL, authToken: TURSO_DB_SECRET_TOKEN})
+const sqlite = createClient({url: dev ? "file:sqlite.db" : TURSO_DB_URL!, authToken: TURSO_DB_SECRET_TOKEN})
 export const db = drizzle(sqlite);
