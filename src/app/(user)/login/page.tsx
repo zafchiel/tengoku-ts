b7db4a59cb6cba24e7ab24fa0f06cbb82faaa9cb
zafchiel <bialakropla127.9@o2.pl>
2024-03-28 20:52:53 +0100

    add comments and format code

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -27,20 +27,24 @@ export default async function SignInPage() {
         <div className="flex flex-col md:grid grid-cols-2 gap-4 max-w-xl">
           <OAuthLink
             providerName="google"
-            icon={(props) => <GoogleIcon {...props} />}
+            icon={GoogleIcon}
           />
           <OAuthLink
             providerName="discord"
-            icon={(props) => <DiscordIcon {...props} />}
+            icon={DiscordIcon}
           />
+          
+          {/* You can pass component reference with default props */}
           <OAuthLink
             providerName="github"
-            icon={(props) => <GithubIcon {...props} />}
+            icon={GithubIcon}
           />
+
+          {/* Passing function to modify default props */}
           <OAuthLink
             providerName="MyAnimeList"
             providerLink="mal"
-            icon={(props) => <MalIcon {...props} />}
+            icon={(props) => <MalIcon className="mx-auto" {...props} />}
           />
         </div>
       </section>
