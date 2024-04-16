d477b3cf9b7ce1a0698d5e11a268afe084de99dc
zafchiel <bialakropla@proton.me>
2024-04-16 21:16:33 +0200

    delete console log

--- src/components/providers/user-info-provider.tsx
+++ src/components/providers/user-info-provider.tsx
@@ -1,57 +1,57 @@
-import { User } from "lucia";
-import { createContext, useState, useEffect } from "react";
+import {User} from "lucia";
+import {createContext, useState, useEffect} from "react";
 import axios from "axios";
 
 type UserContextType = {
-  userInfo: User | null;
-  isAuthenticating: boolean;
-  cleanUserInfo: () => void;
+    userInfo: User | null;
+    isAuthenticating: boolean;
+    cleanUserInfo: () => void;
 };
 
 export const UserInfoContext = createContext<UserContextType>({
-  userInfo: null,
-  isAuthenticating: true,
-  cleanUserInfo: () => {},
+    userInfo: null,
+    isAuthenticating: true,
+    cleanUserInfo: () => {
+    },
 });
 
 type UserInfoProviderProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
-export const UserInfoProvier = ({ children }: UserInfoProviderProps) => {
-  const [isAuthenticating, setIsAuthenticating] = useState(true);
-  const [userInfo, setUserInfo] = useState<User | null>(null);
+export const UserInfoProvier = ({children}: UserInfoProviderProps) => {
+    const [isAuthenticating, setIsAuthenticating] = useState(true);
+    const [userInfo, setUserInfo] = useState<User | null>(null);
 
-  useEffect(() => {
-    async function fetchUserAndSession() {
-      try {
-        const res = await axios.get<User>("/api/user");
+    useEffect(() => {
+        async function fetchUserAndSession() {
+            try {
+                const res = await axios.get<User>("/api/user");
 
-        if (res.data) {
-          setUserInfo(res.data);
-        } else {
-          setUserInfo(null);
+                if (res.data) {
+                    setUserInfo(res.data);
+                } else {
+                    setUserInfo(null);
+                }
+            } catch {
+                setUserInfo(null);
+            } finally {
+                setIsAuthenticating(false);
+            }
         }
-      } catch (error) {
-        console.log(error);
-        setUserInfo(null);
-      } finally {
-        setIsAuthenticating(false);
-      }
+
+        fetchUserAndSession();
+    }, []);
+
+    const value = {
+        userInfo,
+        isAuthenticating,
+        cleanUserInfo: () => setUserInfo(null),
     }
 
-    fetchUserAndSession();
-  }, []);
-
-  const value = {
-    userInfo,
-    isAuthenticating,
-    cleanUserInfo: () => setUserInfo(null),
-  }
-
-  return (
-    <UserInfoContext.Provider value={value}>
-      {children}
-    </UserInfoContext.Provider>
-  );
+    return (
+        <UserInfoContext.Provider value={value}>
+            {children}
+        </UserInfoContext.Provider>
+    );
 };
