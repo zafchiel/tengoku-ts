c6980ebc7490db59ec2c6978241e723e75bd2e98
zafchiel <bialakropla@proton.me>
2024-04-18 22:53:08 +0200

    refactor: remove pointless filtering of relations

--- src/components/detailsPage/relations-sections.tsx
+++ src/components/detailsPage/relations-sections.tsx
@@ -1,41 +1,17 @@
 import { AnimeInfo } from "@/types";
 import RelationCategory from "@/components/detailsPage/relation-category";
 
-const filterRelations = (anime: AnimeInfo) => {
-    const result: {
-        [key: string]: {
-            mal_id: number;
-            type: string;
-            name: string;
-            url: string;
-        }[];
-    } = {};
-    for (const v of anime.relations) {
-        const key: string = v.relation;
-        if (key in result) {
-            result[key].push(...v.entry);
-        } else {
-            result[key] = [];
-            result[key].push(...v.entry);
-        }
-    }
-
-    return Object.entries(result);
-};
-
 type RelationSectionProps = {
     animeInfo: AnimeInfo;
 }
 
 export default function RelationsSection({ animeInfo }: RelationSectionProps) {
-    const relations = filterRelations(animeInfo);
-
     return (
         <section>
             <h3 className="text-3xl font-semibold">Relations</h3>
             <div className="space-y-3 px-1 py-2">
-                {relations.map((category) => (
-                    <RelationCategory category={category[0]} entries={category[1]} key={category[0]}/>
+                {animeInfo.relations.map((relation) => (
+                    <RelationCategory category={relation.relation} entries={relation.entry} key={relation.relation}/>
                 ))}
             </div>
         </section>
