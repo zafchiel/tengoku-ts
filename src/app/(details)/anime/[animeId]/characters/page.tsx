e24a9acc6e3beb5d9253d42340cee16728a1e954
zafchiel <bialakropla@proton.me>
2024-04-21 19:14:40 +0200

    display characters page

--- src/app/(details)/anime/[animeId]/characters/page.tsx
+++ src/app/(details)/anime/[animeId]/characters/page.tsx
@@ -1,11 +1,28 @@
+import { fetchAnimeCharacters, fetchAnimeInfoFull } from "@/lib/utils";
+import CharacterCard from "@/components/detailsPage/character-card";
+
 type AllCharactersPageProps = {
     params: {
         animeId: string;
     }
 }
 
-export default async function AllCharactersPage({ params: { animeId }}: AllCharactersPageProps) {
+export default async function AllCharactersPage({ params: { animeId } }: AllCharactersPageProps) {
+    const promises = [
+        fetchAnimeCharacters(animeId),
+        fetchAnimeInfoFull(animeId)
+    ] as const;
+
+    const [characters, animeInfo] = await Promise.all(promises);
     return (
-        {animeId}
-    )
+        <main className="container space-y-8 px-2 md:px-4 md:py-14 md:mt-10">
+            <h2 className="text-5xl"><span className="text-muted-foreground">characters: </span>{animeInfo.title}</h2>
+
+            <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                {characters.map((character) => (
+                    <CharacterCard key={character.character.mal_id} character={character}/>
+                ))}
+            </section>
+        </main>
+    );
 }
