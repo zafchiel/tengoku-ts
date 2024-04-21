507e7fbb55b08e12fed2c54b8527d9d8da5fca3f
zafchiel <bialakropla@proton.me>
2024-04-21 19:32:09 +0200

    display all actors on characters page

--- src/app/(details)/anime/[animeId]/characters/page.tsx
+++ src/app/(details)/anime/[animeId]/characters/page.tsx
@@ -1,5 +1,5 @@
 import { fetchAnimeCharacters, fetchAnimeInfoFull } from "@/lib/utils";
-import CharacterCard from "@/components/detailsPage/character-card";
+import CharacterCardFull from "@/components/characters-page/character-card-full";
 
 type AllCharactersPageProps = {
     params: {
@@ -16,11 +16,12 @@ export default async function AllCharactersPage({ params: { animeId } }: AllChar
     const [characters, animeInfo] = await Promise.all(promises);
     return (
         <main className="container space-y-8 px-2 md:px-4 md:py-14 md:mt-10">
-            <h2 className="text-5xl"><span className="text-muted-foreground">characters: </span>{animeInfo.title}</h2>
+            <h2 className="text-3xl md:text-5xl"><span
+                className="text-muted-foreground">characters: </span>{animeInfo.title}</h2>
 
-            <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-sm md:text-base">
                 {characters.map((character) => (
-                    <CharacterCard key={character.character.mal_id} character={character}/>
+                    <CharacterCardFull key={character.character.mal_id} character={character}/>
                 ))}
             </section>
         </main>
