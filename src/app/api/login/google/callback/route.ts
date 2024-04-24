37ab0a569895cb15e114fb2aa715063de4f1dde3
zafchiel <bialakropla@proton.me>
2024-04-24 18:03:49 +0200

    update lucia and fix login redirect path

--- src/app/api/login/google/callback/route.ts
+++ src/app/api/login/google/callback/route.ts
@@ -1,104 +1,104 @@
 import { lucia, googleOAuth } from "@/lib/server/auth";
 import { cookies } from "next/headers";
 import { OAuth2RequestError } from "arctic";
-import { generateId } from "lucia";
+import { generateIdFromEntropySize } from "lucia";
 import { db } from "@/lib/server/db";
 import { userTable } from "@/lib/server/db/schema";
 import { and, eq } from "drizzle-orm";
 import axios from "axios";
 
 export async function GET(request: Request): Promise<Response> {
-  const url = new URL(request.url);
-  const code = url.searchParams.get("code");
-  const state = url.searchParams.get("state");
+    const url = new URL(request.url);
+    const code = url.searchParams.get("code");
+    const state = url.searchParams.get("state");
 
-  const redirectLocation = cookies().get('redirect')?.value ?? "/profile";
-  cookies().delete('redirect');
+    const redirectLocation = cookies().get('redirect')?.value ?? "/user";
+    cookies().delete('redirect');
 
-  const storedVerifier = cookies().get("google_oauth_code_verifier")?.value ?? null;
-  const storedState = cookies().get("google_oauth_state")?.value ?? null;
+    const storedVerifier = cookies().get("google_oauth_code_verifier")?.value ?? null;
+    const storedState = cookies().get("google_oauth_state")?.value ?? null;
 
-  if (!code || !storedVerifier || !storedState || !state || state !== storedState) {
-    return new Response(null, {
-      status: 400,
-    });
-  }
+    if (!code || !storedVerifier || !storedState || !state || state !== storedState) {
+        return new Response(null, {
+            status: 400,
+        });
+    }
 
-  try {
-    const tokens = await googleOAuth.validateAuthorizationCode(code, storedVerifier);
+    try {
+        const tokens = await googleOAuth.validateAuthorizationCode(code, storedVerifier);
 
-    const { data: userInfo } = await axios.get<GoogleUserInfo>("https://openidconnect.googleapis.com/v1/userinfo", {
-        headers: {
-            Authorization: `Bearer ${tokens.accessToken}`
+        const { data: userInfo } = await axios.get<GoogleUserInfo>("https://openidconnect.googleapis.com/v1/userinfo", {
+            headers: {
+                Authorization: `Bearer ${tokens.accessToken}`
+            }
+        });
+
+        const existingUser = await db
+            .select()
+            .from(userTable)
+            .where(
+                and(
+                    eq(userTable.authProviderType, "google"),
+                    eq(userTable.authProviderId, userInfo.sub)
+                )
+            )
+            .get();
+
+        if (existingUser) {
+            // Create session cookie for exiting user
+            const session = await lucia.createSession(existingUser.id, {});
+            const sessionCookie = lucia.createSessionCookie(session.id);
+            cookies().set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            );
+            return new Response(null, {
+                status: 302,
+                headers: {
+                    Location: redirectLocation,
+                },
+            });
         }
-    })
 
-    const existingUser = await db
-      .select()
-      .from(userTable)
-      .where(
-        and(
-          eq(userTable.authProviderType, "google"),
-          eq(userTable.authProviderId, userInfo.sub)
-        )
-      )
-      .get();
+        // If new user, create a new user in the database
 
-    if (existingUser) {
-      // Create session cookie for exiting user
-      const session = await lucia.createSession(existingUser.id, {});
-      const sessionCookie = lucia.createSessionCookie(session.id);
-      cookies().set(
-        sessionCookie.name,
-        sessionCookie.value,
-        sessionCookie.attributes
-      );
-      return new Response(null, {
-        status: 302,
-        headers: {
-          Location: redirectLocation,
-        },
-      });
+        const userId = generateIdFromEntropySize(10);
+
+        await db.insert(userTable).values({
+            id: userId,
+            username: userInfo.name,
+            authProviderType: "google",
+            authProviderId: userInfo.sub
+        });
+
+        // Create session cookie for new user
+        const session = await lucia.createSession(userId, {});
+        const sessionCookie = lucia.createSessionCookie(session.id);
+        cookies().set(
+            sessionCookie.name,
+            sessionCookie.value,
+            sessionCookie.attributes
+        );
+        return new Response(null, {
+            status: 302,
+            headers: {
+                Location: redirectLocation,
+            },
+        });
+    } catch (e) {
+        console.log(e);
+        // the specific error message depends on the provider
+        if (e instanceof OAuth2RequestError) {
+            // invalid code
+            return new Response(e.message, {
+                status: 400,
+            });
+        }
+        return new Response(null, {
+            status: 500,
+        });
     }
-
-    // If new user, create a new user in the database
-
-    const userId = generateId(15);
-
-    await db.insert(userTable).values({
-      id: userId,
-      username: userInfo.name,
-      authProviderType: "google",
-      authProviderId: userInfo.sub
-    });
-
-    // Create session cookie for new user
-    const session = await lucia.createSession(userId, {});
-    const sessionCookie = lucia.createSessionCookie(session.id);
-    cookies().set(
-      sessionCookie.name,
-      sessionCookie.value,
-      sessionCookie.attributes
-    );
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: redirectLocation,
-      },
-    });
-  } catch (e) {
-    console.log(e)
-    // the specific error message depends on the provider
-    if (e instanceof OAuth2RequestError) {
-      // invalid code
-      return new Response(e.message, {
-        status: 400,
-      });
-    }
-    return new Response(null, {
-      status: 500,
-    });
-  }
 }
 
 interface GoogleUserInfo {
@@ -110,4 +110,4 @@ interface GoogleUserInfo {
     email: string;
     email_verified: boolean,
     locale: string;
-  }
+}
