f94838fba5e076ead3b87579c104ffa502e59a78
zafchiel <bialakropla@proton.me>
2024-08-22 20:35:13 +0200

    refactor: don't retry fetching user info

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -1,16 +1,18 @@
-import axios from 'axios';
-import type { User } from 'lucia';
-import useSWR from 'swr';
+import axios from "axios";
+import type { User } from "lucia";
+import useSWR from "swr";
 
 const fetcher = (url: string) => axios.get<User>(url).then((res) => res.data);
 
 export default function useUser() {
-  const { data, error, isLoading, mutate } = useSWR('/api/user', fetcher);
+	const { data, error, isLoading, mutate } = useSWR("/api/user", fetcher, {
+		shouldRetryOnError: false,
+	});
 
-  return {
-    user: data,
-    isAuthenticating: isLoading,
-    error,
-    mutate,
-  };
+	return {
+		user: data,
+		isAuthenticating: isLoading,
+		error,
+		mutate,
+	};
 }
