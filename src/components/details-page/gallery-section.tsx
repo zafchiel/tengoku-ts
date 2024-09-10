e1322ccdd790d66787e763f2ca507e93a6add7e9
zafchiel <bialakropla@proton.me>
2024-09-10 19:38:33 +0200

    fix: render alert on empty gallery

--- src/components/details-page/gallery-section.tsx
+++ src/components/details-page/gallery-section.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { JIKAN_API_URL } from "@/lib/constants";
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 import { AlertCircle } from "lucide-react";
@@ -34,6 +34,21 @@ export default function GallerySection({ animeId }: GallerySectionProps) {
 	const [isLoading, setIsLoading] = useState(false);
 	const sectionRef = useRef<HTMLElement>(null);
 
+	const fetchGallery = useCallback(async (): Promise<void> => {
+		try {
+			const response = await axios.get(
+				`${JIKAN_API_URL}/anime/${animeId}/pictures`,
+			);
+			setGallery(response.data.data);
+		} catch (error) {
+			console.error("Error fetching gallery:", error);
+			setGallery(null);
+		} finally {
+			setIsLoading(false);
+		}
+	}, [animeId]);
+
+
 	useEffect(() => {
 		const observer = new IntersectionObserver(
 			([entry]) => {
@@ -46,30 +61,19 @@ export default function GallerySection({ animeId }: GallerySectionProps) {
 			{ threshold: 0, rootMargin: "0px 0px 200px 0px" },
 		);
 
-		if (sectionRef.current) {
-			observer.observe(sectionRef.current);
+		const section = sectionRef.current;
+
+		if (section) {
+			observer.observe(section);
 		}
 
 		return () => {
-			if (sectionRef.current) {
-				observer.unobserve(sectionRef.current);
+			if (section) {
+				observer.unobserve(section);
 			}
 		};
-	}, [gallery, isLoading]);
+	}, [gallery, isLoading, fetchGallery]);
 
-	const fetchGallery = async () => {
-		try {
-			const response = await axios.get(
-				`${JIKAN_API_URL}/anime/${animeId}/pictures`,
-			);
-			setGallery(response.data.data);
-		} catch (error) {
-			console.error("Error fetching gallery:", error);
-			setGallery(null);
-		} finally {
-			setIsLoading(false);
-		}
-	};
 
 	return (
 		<section id="gallery" className="scroll-mt-40" ref={sectionRef}>
@@ -77,7 +81,7 @@ export default function GallerySection({ animeId }: GallerySectionProps) {
 			<hr className="mb-2" />
 			{isLoading ? (
 				<Skeleton className="w-full h-96" />
-			) : !gallery ? (
+			) : !gallery || gallery.length < 1 ? (
 				<Alert variant="default" className="m-2">
 					<AlertCircle className="h-4 w-4" />
 					<AlertTitle>No pictures</AlertTitle>
