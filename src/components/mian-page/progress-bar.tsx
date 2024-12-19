3f98b38334fce559fad6cab121e52c605723c684
zafchiel <bialakropla@proton.me>
2024-12-19 20:34:47 +0100

    move progressbar to side

--- src/components/mian-page/progress-bar.tsx
+++ src/components/mian-page/progress-bar.tsx
@@ -1,8 +1,8 @@
 export default function Progressbar({ barWidth }: { barWidth: number }) {
-	return (
-		<div
-			className="bg-white fixed top-0 left-0 h-1 z-50"
-			style={{ width: barWidth + "%" }}
-		></div>
-	);
+  return (
+    <div
+      className="bg-white fixed bottom-0 left-0 w-1 z-50"
+      style={{ height: barWidth + "%" }}
+    ></div>
+  );
 }
