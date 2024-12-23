3b2721a3a4bce5b089dc4f2ad05d948b8eeb9ebe
zafchiel <bialakropla@proton.me>
2024-12-23 12:24:20 +0100

    embla preset carousel

--- src/app/(main)/new/page.tsx
+++ src/app/(main)/new/page.tsx
@@ -1,15 +1,48 @@
-import NewCarousel from "@/components/mian-page/new-carousel";
+import NewCarousel from "@/components/mian-page/carousel";
+import type { AnimeInfoFiltered, AnimeInfo } from "@/types";
+import { propertiesToKeep } from "@/types";
+import { JIKAN_API_TOP_ANIME_URL } from "@/lib/constants";
 
 export default async function NewMainPage() {
-return (
-  <main className="py-14 px-6 grid grid-cols-1 md:grid-cols-[400px_1fr] min-h-screen items-center gap-8">
-    <div>
-      <h1 className="text-6xl font-bold">Bleach: Sennen Kessen-hen - Soukoku-tan</h1>
-      <p className="text-muted-foreground">
-        Bleach: Sennen Kessen-hen - Soukoku-tan
-      </p>
-    </div>
-    <NewCarousel />
-  </main>
-)
+	let topAiringData: AnimeInfoFiltered[] = [];
+	try {
+		const response = await fetch(
+			`${JIKAN_API_TOP_ANIME_URL}?filter=airing&limit=6`,
+			{
+				// Revalidate every 7 days
+				next: { revalidate: 60 * 60 * 24 * 7 },
+			},
+		);
+		if (!response.ok) {
+			throw new Error(`HTTP Error! status: ${response.status}`);
+		}
+		const data = (await response.json()).data as AnimeInfo[];
+
+		topAiringData = data.map((anime) => {
+			const newItem: AnimeInfoFiltered = {};
+			for (const property of propertiesToKeep) {
+				if (Object.hasOwn(anime, property)) {
+					// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+					(newItem as any)[property] = anime[property];
+				}
+			}
+			return newItem;
+		});
+	} catch (error) {
+		console.log(error);
+		topAiringData = [];
+	}
+	return (
+		<main className="py-14 px-6 grid grid-cols-1 md:grid-cols-[400px_1fr] min-h-screen items-center gap-8">
+			<div>
+				<h1 className="text-6xl font-bold">
+					Bleach: Sennen Kessen-hen - Soukoku-tan
+				</h1>
+				<p className="text-muted-foreground">
+					Bleach: Sennen Kessen-hen - Soukoku-tan
+				</p>
+			</div>
+			<NewCarousel slides={[1, 2, 3, 4, 5, 6, 7]} options={{ loop: true }} />
+		</main>
+	);
 }
---
+++ src/components/mian-page/carousel.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React, { useCallback } from "react";
+import type { EmblaOptionsType, EmblaCarouselType } from "embla-carousel";
+import { DotButton, useDotButton } from "./carousel-dot-button";
+import {
+	PrevButton,
+	NextButton,
+	usePrevNextButtons,
+} from "./carousel-arrow-buttons";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import "./carousel.css";
+
+type PropType = {
+	slides: number[];
+	options?: EmblaOptionsType;
+};
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+	const { slides, options } = props;
+	const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+		Autoplay({
+			stopOnInteraction: false,
+			stopOnMouseEnter: true,
+			delay: 2000,
+		}),
+	]);
+
+	const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+		const autoplay = emblaApi?.plugins()?.autoplay;
+		if (!autoplay) return;
+
+		const resetOrStop =
+			autoplay.options.stopOnInteraction === false
+				? autoplay.reset
+				: autoplay.stop;
+
+		resetOrStop();
+	}, []);
+
+	const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
+		emblaApi,
+		onNavButtonClick,
+	);
+
+	const {
+		prevBtnDisabled,
+		nextBtnDisabled,
+		onPrevButtonClick,
+		onNextButtonClick,
+	} = usePrevNextButtons(emblaApi, onNavButtonClick);
+
+	return (
+		<section className="embla">
+			<div className="embla__viewport" ref={emblaRef}>
+				<div className="embla__container">
+					{slides.map((index) => (
+						<div className="embla__slide" key={index}>
+							<div className="embla__slide__number">{index + 1}</div>
+						</div>
+					))}
+				</div>
+			</div>
+
+			<div className="embla__controls">
+				<div className="embla__buttons">
+					<PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+					<NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+				</div>
+
+				<div className="embla__dots">
+					{scrollSnaps.map((_, index) => (
+						<DotButton
+							key={index}
+							onClick={() => onDotButtonClick(index)}
+							className={"embla__dot".concat(
+								index === selectedIndex ? " embla__dot--selected" : "",
+							)}
+						/>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default EmblaCarousel;
